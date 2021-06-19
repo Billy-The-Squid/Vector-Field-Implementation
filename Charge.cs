88fc57bda1cc7efe01f8acd119ee238f8b947992
Will Hanstedt <39772580+Billy-The-Squid@users.noreply.github.com>
2021-06-19 11:46:23 -0500

    Fresh start (non-functional)

--- Charge.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Charge : MonoBehaviour
-{
-    [SerializeField]
-    public float charge;
-}
--- VectorField.cs
+++ VectorField.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(ZoneManager))]
 public class VectorField : MonoBehaviour
 {
     // [SerializeField, Min(1)]
